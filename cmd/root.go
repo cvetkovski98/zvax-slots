ed68e6b33ff641bda4e2701ffafc37ff0286bf4c
Oliver Cvetkovski <oliver.cvetkovski@thethingsindustries.com>
2023-01-10 00:32:24 +0100

    Add config name flag

--- cmd/root.go
+++ cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var cfgFile string
 var root = &cobra.Command{
 	Short: "Slots microservice",
 	Long:  `Slots microservice`,
@@ -17,12 +18,13 @@ var root = &cobra.Command{
 
 func init() {
 	cobra.OnInitialize(configure)
+	root.PersistentFlags().StringVarP(&cfgFile, "config", "c", "config.dev.yml", "config file name")
 	root.AddCommand(runCommand)
 	root.AddCommand(seedCommand)
 }
 
 func configure() {
-	if err := config.LoadConfig("config.dev.yaml"); err != nil {
+	if err := config.LoadConfig(cfgFile); err != nil {
 		panic(err)
 	}
 }
